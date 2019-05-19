661eb35d3f31af29f231660f15af8f68c0c50bce
[peter] <[insanity2017@126.com]>
2019-05-20 00:15:13 +0800

    add new function

--- AES_Improve.c
+++ AES_Improve.c
@@ -41,12 +41,12 @@ unsigned char inv_S[256] = {
 };
 
 /* copy in[16] to state[4][4] */
-int loadStateArray(uint8_t(*state)[4],
-	const uint8_t* in)
+int loadStateArray(uint8_t(*state)[4], 
+	const uint8_t* in) 
 {
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			state[j][i] = *in++;
 		}
@@ -56,12 +56,12 @@ int loadStateArray(uint8_t(*state)[4],
 }
 
 /* copy state[4][4] to out[16] */
-int storeStateArray(uint8_t(*state)[4],
-	uint8_t* out)
+int storeStateArray(uint8_t(*state)[4], 
+	uint8_t* out) 
 {
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			*out++ = state[j][i];
 		}
@@ -71,19 +71,19 @@ int storeStateArray(uint8_t(*state)[4],
 }
 
 //密钥扩展
-int keyExpansion(const uint8_t* key,
-	uint32_t keyLen,
-	AesKey* aesKey)
+int keyExpansion(const uint8_t* key, 
+	uint32_t keyLen, 
+	AesKey* aesKey) 
 {
 
-	if (NULL == key || NULL == aesKey)
+	if (NULL == key || NULL == aesKey) 
 	{
 		printf("keyExpansion param is NULL\n");
 
 		return -1;
 	}
 
-	if (keyLen != 16)
+	if (keyLen != 16) 
 	{
 		printf("keyExpansion keyLen = %d, Not support.\n", keyLen);
 
@@ -96,13 +96,13 @@ int keyExpansion(const uint8_t* key,
 	/* keyLen is 16 Bytes, generate uint32_t W[44]. */
 
 	/* W[0-3] */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
 		LOAD32H(w[i], key + 4 * i);
 	}
 
 	/* W[4-43] */
-	for (int i = 0; i < 10; ++i)
+	for (int i = 0; i < 10; ++i) 
 	{
 		w[4] = w[0] ^ MIX(w[3]) ^ rcon[i];
 		w[5] = w[1] ^ w[4];
@@ -115,10 +115,10 @@ int keyExpansion(const uint8_t* key,
 
 	//解密秘钥矩阵为加密秘钥矩阵的倒序，方便使用，把ek的11个矩阵倒序排列分配给dk作为解密秘钥
 	//即dk[0-3]=ek[41-44], dk[4-7]=ek[37-40]... dk[41-44]=ek[0-3]
-	for (int j = 0; j < 11; ++j)
+	for (int j = 0; j < 11; ++j) 
 	{
 
-		for (int i = 0; i < 4; ++i)
+		for (int i = 0; i < 4; ++i) 
 		{
 			v[i] = w[i];
 		}
@@ -130,15 +130,15 @@ int keyExpansion(const uint8_t* key,
 }
 
 // 轮秘钥加
-int addRoundKey(uint8_t(*state)[4],
-	const uint32_t * key)
+int addRoundKey(uint8_t(*state)[4], 
+	const uint32_t* key) 
 {
 	uint8_t k[4][4];
 
 	/* i: row, j: col */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			k[i][j] = (uint8_t)BYTE(key[j], 3 - i);  /* 把 uint32 key[4] 先转换为矩阵 uint8 k[4][4] */
 			state[i][j] ^= k[i][j];
@@ -149,12 +149,12 @@ int addRoundKey(uint8_t(*state)[4],
 }
 
 //字节替换
-int subBytes(uint8_t(*state)[4])
+int subBytes(uint8_t(*state)[4]) 
 {
 	/* i: row, j: col */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			state[i][j] = S[state[i][j]]; //直接使用原始字节作为S盒数据下标
 		}
@@ -164,12 +164,12 @@ int subBytes(uint8_t(*state)[4])
 }
 
 //逆字节替换
-int invSubBytes(uint8_t(*state)[4])
+int invSubBytes(uint8_t(*state)[4]) 
 {
 	/* i: row, j: col */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			state[i][j] = inv_S[state[i][j]];
 		}
@@ -179,12 +179,12 @@ int invSubBytes(uint8_t(*state)[4])
 }
 
 //行移位
-int shiftRows(uint8_t(*state)[4])
+int shiftRows(uint8_t(*state)[4]) 
 {
 	uint32_t block[4] = { 0 };
 
 	/* i: row */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
 		//便于行循环移位，先把一行4字节拼成uint_32结构，移位后再转成独立的4个字节uint8_t
 		LOAD32H(block[i], state[i]);
@@ -196,12 +196,12 @@ int shiftRows(uint8_t(*state)[4])
 }
 
 //逆行移位
-int invShiftRows(uint8_t(*state)[4])
+int invShiftRows(uint8_t(*state)[4]) 
 {
 	uint32_t block[4] = { 0 };
 
 	/* i: row */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
 		LOAD32H(block[i], state[i]);
 
@@ -215,15 +215,15 @@ int invShiftRows(uint8_t(*state)[4])
 
 /* Galois Field (256) Multiplication of two Bytes */
 // 两字节的伽罗华域乘法运算
-uint8_t GMul(uint8_t u,
-	uint8_t v)
+uint8_t GMul(uint8_t u, 
+	uint8_t v) 
 {
 	uint8_t p = 0;
 
-	for (int i = 0; i < 8; ++i)
+	for (int i = 0; i < 8; ++i) 
 	{
-		if (u & 0x01)
-		{
+		if (u & 0x01) 
+		{    
 			p ^= v;
 		}
 
@@ -231,7 +231,7 @@ uint8_t GMul(uint8_t u,
 
 		v <<= 1;
 
-		if (flag)
+		if (flag) 
 		{
 			v ^= 0x1B; /* x^8 + x^4 + x^3 + x + 1 */
 		}
@@ -243,7 +243,7 @@ uint8_t GMul(uint8_t u,
 }
 
 // 列混合
-int mixColumns(uint8_t(*state)[4])
+int mixColumns(uint8_t(*state)[4]) 
 {
 	uint8_t tmp[4][4];
 	uint8_t M[4][4] = { {0x02, 0x03, 0x01, 0x01},
@@ -252,17 +252,17 @@ int mixColumns(uint8_t(*state)[4])
 					   {0x03, 0x01, 0x01, 0x02} };
 
 	/* copy state[4][4] to tmp[4][4] */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			tmp[i][j] = state[i][j];
 		}
 	}
 
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{  //伽罗华域加法和乘法
 			state[i][j] = GMul(M[i][0], tmp[0][j]) ^ GMul(M[i][1], tmp[1][j])
 				^ GMul(M[i][2], tmp[2][j]) ^ GMul(M[i][3], tmp[3][j]);
@@ -273,7 +273,7 @@ int mixColumns(uint8_t(*state)[4])
 }
 
 // 逆列混合
-int invMixColumns(uint8_t(*state)[4])
+int invMixColumns(uint8_t(*state)[4]) 
 {
 	uint8_t tmp[4][4];
 	uint8_t M[4][4] = { {0x0E, 0x0B, 0x0D, 0x09},
@@ -282,17 +282,17 @@ int invMixColumns(uint8_t(*state)[4])
 					   {0x0B, 0x0D, 0x09, 0x0E} };  //使用列混合矩阵的逆矩阵
 
 	/* copy state[4][4] to tmp[4][4] */
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			tmp[i][j] = state[i][j];
 		}
 	}
 
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) 
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < 4; ++j) 
 		{
 			state[i][j] = GMul(M[i][0], tmp[0][j]) ^ GMul(M[i][1], tmp[1][j])
 				^ GMul(M[i][2], tmp[2][j]) ^ GMul(M[i][3], tmp[3][j]);
@@ -304,11 +304,11 @@ int invMixColumns(uint8_t(*state)[4])
 
 // AES-128加密接口，输入key应为16字节长度，输入长度应该是16字节整倍数，
 // 这样输出长度与输入长度相同，函数调用外部为输出数据分配内存
-int aesEncrypt(const uint8_t * key,
-	uint32_t keyLen,
-	const uint8_t * pt,
-	uint8_t * ct,
-	uint32_t len)
+int aesEncrypt(const uint8_t* key, 
+	uint32_t keyLen, 
+	const uint8_t* pt, 
+	uint8_t* ct, 
+	uint32_t len) 
 {
 
 	AesKey aesKey;
@@ -318,21 +318,21 @@ int aesEncrypt(const uint8_t * key,
 	uint8_t actualKey[16] = { 0 };
 	uint8_t state[4][4] = { 0 };
 
-	if (NULL == key || NULL == pt || NULL == ct)
+	if (NULL == key || NULL == pt || NULL == ct) 
 	{
 		printf("param err.\n");
 
 		return -1;
 	}
 
-	if (keyLen > 16)
+	if (keyLen > 16) 
 	{
 		printf("keyLen must be 16.\n");
 
 		return -1;
 	}
 
-	if (len % BLOCKSIZE)
+	if (len % BLOCKSIZE) 
 	{
 		printf("inLen is invalid.\n");
 
@@ -349,21 +349,26 @@ int aesEncrypt(const uint8_t * key,
 		loadStateArray(state, pt);
 
 		// 轮秘钥加
-		addRoundKey(state, rk);
+		//addRoundKey(state, rk);
 
-		for (int j = 1; j < 10; ++j)
+		for (int j = 1; j < 2; ++j) 
 		{
 			rk += 4;
+			/*
 			subBytes(state);   // 字节替换
 			shiftRows(state);  // 行移位
 			mixColumns(state); // 列混合
-			addRoundKey(state, rk); // 轮秘钥加
+			addRoundKey(state, rk); // 轮秘钥加			
+			*/
+
+			encOperationMergece(state, rk);
+
 		}
 
-		subBytes(state);    // 字节替换
-		shiftRows(state);  // 行移位
-		// 此处不进行列混合
-		addRoundKey(state, rk + 4); // 轮秘钥加
+		//subBytes(state);    // 字节替换
+		//shiftRows(state);  // 行移位
+		//// 此处不进行列混合
+		//addRoundKey(state, rk + 4); // 轮秘钥加
 
 		// 把4x4状态矩阵转换为uint8_t一维数组输出保存
 		storeStateArray(state, pos);
@@ -377,11 +382,11 @@ int aesEncrypt(const uint8_t * key,
 }
 
 // AES128解密， 参数要求同加密
-int aesDecrypt(const uint8_t * key,
+int aesDecrypt(const uint8_t* key, 
 	uint32_t keyLen,
-	const uint8_t * ct,
-	uint8_t * pt,
-	uint32_t len)
+	const uint8_t* ct, 
+	uint8_t* pt, 
+	uint32_t len) 
 {
 	AesKey aesKey;
 	uint8_t* pos = pt;
@@ -390,19 +395,19 @@ int aesDecrypt(const uint8_t * key,
 	uint8_t actualKey[16] = { 0 };
 	uint8_t state[4][4] = { 0 };
 
-	if (NULL == key || NULL == ct || NULL == pt)
+	if (NULL == key || NULL == ct || NULL == pt) 
 	{
 		printf("param err.\n");
 		return -1;
 	}
 
-	if (keyLen > 16)
+	if (keyLen > 16) 
 	{
 		printf("keyLen must be 16.\n");
 		return -1;
 	}
 
-	if (len % BLOCKSIZE)
+	if (len % BLOCKSIZE) 
 	{
 		printf("inLen is invalid.\n");
 		return -1;
@@ -416,21 +421,25 @@ int aesDecrypt(const uint8_t * key,
 		// 把16字节的密文转换为4x4状态矩阵来进行处理
 		loadStateArray(state, ct);
 		// 轮秘钥加，同加密
-		addRoundKey(state, rk);
+		//addRoundKey(state, rk);
 
-		for (int j = 1; j < 10; ++j)
+		for (int j = 1; j < 2; ++j) 
 		{
 			rk += 4;
+			/*
 			invShiftRows(state);    // 逆行移位
 			invSubBytes(state);     // 逆字节替换，这两步顺序可以颠倒
 			addRoundKey(state, rk); // 轮秘钥加，同加密
 			invMixColumns(state);   // 逆列混合
+			*/
+
+			decOperationMergece(state, rk);
 		}
 
-		invSubBytes(state);   // 逆字节替换
-		invShiftRows(state);  // 逆行移位
-		// 此处没有逆列混合
-		addRoundKey(state, rk + 4);  // 轮秘钥加，同加密
+		//invSubBytes(state);   // 逆字节替换
+		//invShiftRows(state);  // 逆行移位
+		//// 此处没有逆列混合
+		//addRoundKey(state, rk + 4);  // 轮秘钥加，同加密
 
 		storeStateArray(state, pos);  // 保存明文数据
 		pos += BLOCKSIZE;  // 输出数据内存指针移位分组长度
@@ -441,14 +450,13 @@ int aesDecrypt(const uint8_t * key,
 	return 0;
 }
 
-int OperationMergence(uint8_t(*state)[4])
+int encOperationMergece(uint8_t(*state)[4],const uint32_t* key)
 {
 	uint8_t tmp[4][4];
 	uint8_t M[4][4] = { {0x02, 0x03, 0x01, 0x01},
 					   {0x01, 0x02, 0x03, 0x01},
 					   {0x01, 0x01, 0x02, 0x03},
 					   {0x03, 0x01, 0x01, 0x02} };
-
 	/* copy state[4][4] to tmp[4][4] */
 	for (int i = 0; i < 4; ++i)
 	{
@@ -458,32 +466,168 @@ int OperationMergence(uint8_t(*state)[4])
 		}
 	}
 
+	//轮加密
+	uint8_t k[4][4];
+	//利用空间换时间
+	uint8_t a[4];
 	/* i: row, j: col */
 	for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 4; ++j)
 		{
-			state[i][j] = S[state[i][j]]; //直接使用原始字节作为S盒数据下标
+			k[i][j] = (uint8_t)BYTE(key[j], 3 - i);  /* 把 uint32 key[4] 先转换为矩阵 uint8 k[4][4] */
+
+			switch (i)
+			{
+			case 0:
+				//直接使用原始字节作为S
+				a[0] = S[tmp[0][0]];
+				a[1] = S[tmp[1][1]];
+				a[2] = S[tmp[2][2]];
+				a[3] = S[tmp[3][3]];
+
+				//M矩阵的第一行乘以行移后tem矩阵的第一列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			case 1:
+				//直接使用原始字节作为S,将字节替换和行变换合并在一起
+				a[0] = S[tmp[0][1]];
+				a[1] = S[tmp[1][2]];
+				a[2] = S[tmp[2][3]];
+				a[3] = S[tmp[3][0]];
+
+				//M矩阵的第二行乘以行移后tem矩阵的第二列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			case 2:
+				//直接使用原始字节作为S,将字节替换和行变换合并在一起
+				a[0] = S[tmp[0][2]];
+				a[1] = S[tmp[1][3]];
+				a[2] = S[tmp[2][0]];
+				a[3] = S[tmp[3][1]];
+
+				//M矩阵的第三行乘以行移后tem矩阵的第三列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			case 3:
+				//直接使用原始字节作为S,将字节替换和行变换合并在一起
+				a[0] = S[tmp[0][3]];
+				a[1] = S[tmp[1][0]];
+				a[2] = S[tmp[2][1]];
+				a[3] = S[tmp[3][2]];
+
+				//M矩阵的第四行乘以行移后tem矩阵的第四列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			default:
+				printf("switch结束：\n");
+				break;
+			}
 		}
 	}
+	return 0;
+}
 
-	uint32_t block[4] = { 0 };
-
-	/* i: row */
-	for (int i = 0; i < 4; ++i)
-	{
-		//便于行循环移位，先把一行4字节拼成uint_32结构，移位后再转成独立的4个字节uint8_t
-		LOAD32H(block[i], state[i]);
-		block[i] = ROF32(block[i], 8 * i);
-		STORE32H(block[i], state[i]);
-	}
-
+int decOperationMergece(uint8_t(*state)[4], const uint32_t* key)
+{
+	uint8_t tmp[4][4];
+	uint8_t M[4][4] = { {0x0E, 0x0B, 0x0D, 0x09},
+					   {0x09, 0x0E, 0x0B, 0x0D},
+					   {0x0D, 0x09, 0x0E, 0x0B},
+					   {0x0B, 0x0D, 0x09, 0x0E} };  //使用列混合矩阵的逆矩阵
+	/* copy state[4][4] to tmp[4][4] */
 	for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 4; ++j)
-		{  //伽罗华域加法和乘法
-			state[i][j] = GMul(M[i][0], tmp[0][j]) ^ GMul(M[i][1], tmp[1][j])
-				^ GMul(M[i][2], tmp[2][j]) ^ GMul(M[i][3], tmp[3][j]);
+		{
+			tmp[i][j] = state[i][j];
+		}
+	}
+
+	//轮加密
+	uint8_t k[4][4];
+	//利用空间换时间
+	uint8_t a[4];
+	/* i: row, j: col */
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			k[i][j] = (uint8_t)BYTE(key[j], 3 - i);  /* 把 uint32 key[4] 先转换为矩阵 uint8 k[4][4] */
+
+			switch (i)
+			{
+			case 0:
+				//直接使用原始字节作为S
+				a[0] = inv_S[tmp[0][0]];
+				a[1] = inv_S[tmp[1][3]];
+				a[2] = inv_S[tmp[2][2]];
+				a[3] = inv_S[tmp[3][1]];
+
+				//轮密钥加
+				a[0] ^= M[0][0];//供参考
+				state[i][j] ^= k[i][j];
+
+				//M矩阵的第一行乘以行移后tem矩阵的第一列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+
+				break;
+			case 1:
+				//直接使用原始字节作为S,将字节替换和行变换合并在一起
+				a[0] = S[tmp[0][1]];
+				a[1] = S[tmp[1][0]];
+				a[2] = S[tmp[2][3]];
+				a[3] = S[tmp[3][2]];
+
+				//M矩阵的第二行乘以行移后tem矩阵的第二列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			case 2:
+				//直接使用原始字节作为S,将字节替换和行变换合并在一起
+				a[0] = S[tmp[0][2]];
+				a[1] = S[tmp[1][1]];
+				a[2] = S[tmp[2][0]];
+				a[3] = S[tmp[3][3]];
+
+				//M矩阵的第三行乘以行移后tem矩阵的第三列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			case 3:
+				//直接使用原始字节作为S,将字节替换和行变换合并在一起
+				a[0] = S[tmp[0][3]];
+				a[1] = S[tmp[1][2]];
+				a[2] = S[tmp[2][1]];
+				a[3] = S[tmp[3][0]];
+
+				//M矩阵的第四行乘以行移后tem矩阵的第四列
+				state[i][j] = GMul(M[i][0], a[0]) ^ GMul(M[i][1], a[1])
+					^ GMul(M[i][2], a[2]) ^ GMul(M[i][3], a[3]);
+				//轮密钥加
+				state[i][j] ^= k[i][j];
+				break;
+			default:
+				printf("switch结束：\n");
+				break;
+			}
 		}
 	}
 	return 0;
